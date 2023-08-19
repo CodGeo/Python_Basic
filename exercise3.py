002722f9ba433e169032f66a5e0f1d1bb72ffcd5
CodGeo <142612798+CodGeo@users.noreply.github.com>
2023-08-20 00:09:45 +0530

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,16 @@
+#Pgm to compute the pay of employees 1.5 times the hourly rates if hours worked is > 40
+
+hrs=input('Please enter your Hours:')
+rate=input('Please enter your Hourly rates:')
+hrs1=float(hrs)
+rate1=float(rate)
+if hrs1>40:
+  ot=hrs1-40
+  ot1=float(ot)
+  pay=(40*rate1)+((ot1*1.5)*10)
+  print('Your Pay is:',pay)
+else: 
+  pay=hrs1*rate1
+  print('Your Pay is:',pay)
+  
+print('All Done')
