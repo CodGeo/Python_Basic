0a6a0c620233023af8500e1a851382548f873176
CodGeo <142612798+CodGeo@users.noreply.github.com>
2023-08-20 00:17:27 +0530

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -5,11 +5,13 @@ rate=input('Please enter your Hourly rates:')
 hrs1=float(hrs)
 rate1=float(rate)
 if hrs1>40:
+  print('Overtime')
   ot=hrs1-40
   ot1=float(ot)
   pay=(40*rate1)+((ot1*1.5)*10)
   print('Your Pay is:',pay)
 else: 
+  print('Regular')
   pay=hrs1*rate1
   print('Your Pay is:',pay)
   
