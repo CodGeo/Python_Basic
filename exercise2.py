696f5e22b06dabe4f38a14968d1911c61733443d
CodGeo <142612798+CodGeo@users.noreply.github.com>
2023-08-20 00:09:00 +0530

    Create exercise2.py

---
+++ exercise2.py
@@ -0,0 +1,6 @@
+#Program to compute the Pay of employees in accordance to the hours they worked
+
+hrs=input('Enter your Hours:')
+rate=input('Enter your Hourly rate in $:')
+pay=float(hrs)*float(rate)
+print('Your pay is: ',pay)
